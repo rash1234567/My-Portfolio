14c74327abdac9355d304625ff343217c84fb111
rash1234567 <imamrasheedatahmad1993@gmail.com>
2022-11-28 08:45:01 -0800

    done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./footer.css"
-import {FaFacebook} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
 import {FiInstagram} from 'react-icons/fi'
 import {BsTwitter} from 'react-icons/bs'
 
@@ -19,9 +19,9 @@ const footer = () => {
         </ul>
 
         <div className='footer__socials'>
-          <a href="https://www.facebook.com/imamrasheedat/1"><FaFacebook/></a>
-          <a href="https://www.instagram.com/imamrasheedat/"><FiInstagram/></a>
-          <a href="https://twitter.com/_Rasheedharh"><BsTwitter/></a>
+          <a href="https://github.com/rash1234567" target='_blank'><FaGithub/></a>
+          <a href="https://www.instagram.com/imamrasheedat/" target='_blank'><FiInstagram/></a>
+          <a href="https://twitter.com/_Rasheedharh" target='_blank'><BsTwitter/></a>
         </div>
 
         <div className='footer__copyright'>
