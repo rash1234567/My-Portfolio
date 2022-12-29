ddd471d4e930edb49a0467175475be88c3be9f0a
rash1234567 <imamrasheedatahmad1993@gmail.com>
2022-12-28 22:53:02 -0800

    updated

--- src/Projects/data.js
+++ src/Projects/data.js
@@ -12,7 +12,7 @@ export const data =[
     {
         title: 'Apple',
         demo: 'https://majestic-gelato.netlify.app',
-        github: 'https://github.com/Lawdshev/Apple',
+        github: 'https://github.com/rash1234567/Apple-store.git',
         img: Applestore,
     },
     {
@@ -21,16 +21,4 @@ export const data =[
         github: 'https://github.com/rash1234567/NEWVIVIDIO',
         img: vividioz,
     },
-    {
-        title: 'Vidly',
-        demo: '',
-        github: '',
-        img: 'https://lh3.googleusercontent.com/vOMkR_Vd2N9bDNdW7FuI2_dpFLxcFnsXECZUe2g43dS7yu2PpZvQOMUVV5wfTTEXdHo7Bd-dPLZSgbyr3SmacElfdg7iPKCANJwSXxbeULHaL5q5gD4KFl15zA0C-6zPiMIVXoyq3GfVMncJk6B9R5Y4HJrpNZkICQrp6zYw2etenoopDkgJZmOgYfFBV9Xx_2efX8WJfz9rHvWrYfYYlgHLqluIEV0rH47-CCVM_WYj_fy9fn9IoDRSMOA0HRx2sCHmwMR29eINDKp50J61deGCJzoYvJyk01SB5BcLxwscCgDOQfvfHv2rWDweV0LGRUh4EN0nXDP5fx23Q0SyLv9llDxFfM_1EgQzR0tSotww1fsO_rKOJSd0a9FwONM2jGEhakWwBWNt8injM98acLK6Q-jGvJoSNsmk5sUYPd-qqzXT7a2cQtmBzOVw4xC1ZAmDYu2cglvkfOvnfNp8dQ5XY5ANFQHYiEmueoBF5DjYLxBBwsun-F9RJzL010gS2PtdnqG7paAQkdttkpp6b6wWDlNkJiI1yaQTrOPsxMBB_rpxTy6qoEnLlusIRJAKHPnb0b9Z3KqwF1IUV3NlBRhpoONovVTdOsldn_zUU8DMrF-dzZ9VPUrRvyaKGQC1O-0n4YxQ4QxG3o3uFyYF301JEQSZWmZkyO_QSA1rEwsuCXZG5jlZjBdCPAoPHqWVWh05s5mWy2EmorTL4SsoWi5ji8HkQlsnjiRwklsCvUk7waCI50cbuipY-sFPkd1-Kju-W57OQy1kYnT4MzyLBTADY2rpjOnMHUaGT73GOdoP2eM1OffvKkh5jePGMFPCL4TAleZogWeU162CiOvOtF0wOgje_odxb-bVFW8TJ88tBzPE5TteU7iKMsVwV_LTLn2EK-FNu6fb8UMer1tN31WpkO5xVAlJrLoX6bp2jc8DR3Hrmi3CJa_rv8rO16QaVPf1X4yG65aoAe1B4g=w1277-h656-no?authuser=0',
-    },
-    {
-        title: 'CountriesQQ',
-        demo: 'https://countriesQQ.onrender.com',
-        github: 'https://github.com/Lawdshev/vatebra_test',
-        img: 'https://lh3.googleusercontent.com/jRl3Dfbap5BRoB_W3bAqVtr12MVWe4Ecoaw3rOb5sGm1qLaWTDQgQvDZ3GS3vhQKZNFno3K2l1n-VsgQFTJmwBFjs9iGGYsA710vCSaj0HlUBHoMHogJ4E-7z395VdGAFkhSF2C8Ms3XKDeaOGQ6AzmQAxuIBLMpgk0tsT1u0JP1GMtq_8_hdgfyF_LvKwnMM37BIjHNmF7cjvwqbFloS2VjQ5Zh8A1Iow9-tzfcDOWYj2a4_J1I1jlA2Q8xxZ3XyOleZ1DwYN8BonTOZGfUSdEOXZzAP_mVUYDJ6RRfSm4KNLjCf4V0Sqtor6oBuZLZkickt6E2LExx8aQiXyk7JXhYjAxVFKX-D8AFm-4ZZlVc0fFuSI4m1Mlk6Ede4kzIyAMQmoxvE3rSkayVSHWL1wYeKlPW7t8dSh4Gd0vmwZw6hi1zxfpoq_EbdBIhUf9enn9g9S3PVCcvZ8YQOsqDhXsSXbhQDntCEYmwJ25s0Hw_8RzmOnWLcNHFEaL3hUHPVL4JtoEsgVjzRUOtspei3K19nRKDBN7u1nm6kwO0Gxuzb_Tr3B7BFHgs2heDuELM7484hwIYK8XHAtR6CPc8bGjlYzdsXKd8tm7pkt0ByvLPc8iayuLydN5oxa5gOzakyYflPJtxR7Ph6F7eBQM3RkWQyiMko8ktHfpMUEMsCPsz8FpiHM_DehdmaTxDWqYugVHCFNN2qAjKgfsb4oIoO1UaU5Qr5fUfiqUFH6Z7vf1XjWFVz8h6P3IeMxwEkyrqruKgTDNo5uqdCUXmDqXbewg6ggHpCRWPYsaGV13hdv0a88_nX57rOD1bgKqArK6H04n8hyE4PsyA1H9b8-mEfcwUqqABo7nKQMbzLAuKfUBTNWsIY2B95xRObQK3q3AOG1eyQeC2wC-MTYf4WYP1VeaOFAPjdDxq8Tctboqa66NyUUDY_zdWgrn60TpH-HjF-TQL4snFt0pyjNfTgw=w578-h611-no?authuser=0',
-    }
 ]
