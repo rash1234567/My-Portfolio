6c106ec735f0307974b44c3972ebc800dcd6b631
rash1234567 <imamrasheedatahmad1993@gmail.com>
2023-01-05 04:44:36 -0800

    new commit

--- src/Projects/data.js
+++ src/Projects/data.js
@@ -38,5 +38,23 @@ export const data = [
         github: 'https://github.com/rash1234567/real-estate.git',
         img: realestate,
     },
+    {
+        title: 'Vatdoc',
+        demo: 'https://sage-duckanoo-db10a2.netlify.app/',
+        github: 'https://github.com/Lawdshev/Vatdoc.git',
+        img: vatdoc,
+    },
+    {
+        title: 'Verifier',
+        demo: 'https://iridescent-speculoos-a7e6ea.netlify.app/',
+        github: 'https://github.com/Lawdshev/Everifyer.git',
+        img: verifier,
+    },
+    {
+        title: 'Itemify',
+        demo: 'https://cosmic-dolphin-db8c7a.netlify.app/',
+        github: 'https://github.com/Lawdshev/Itemify.git',
+        img: itemify,
+    },
 
 ]
